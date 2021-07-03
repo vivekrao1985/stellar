484c2fc7634f241aa31d5a2a2218b201f8f0e542
vivekrao1985 <vivekrao1985@gmail.com>
2021-07-03 16:16:03 -0400

    added likes endpoint

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # stellar
 assignment from stellar
+
+# Setup the environment
+For the purpose of this project I'm using flask and venv for the API endpoints and environment setup.
+
+For setup -
+```
+./setup.sh
+```
+This will create a virtual environment with all requirements installed
+
+To run -
+```
+(optional) source env/bin/activate
+python api/api.py
+```
+
+Additional feature -
+Added a like API endpoint
--- api/snippet.py
+++ api/snippet.py
@@ -5,7 +5,6 @@ from datetime import datetime, timedelta
 snippet = Blueprint('snippet', __name__)
 dic = {}
 
-# name, expires_in, snippet
 @snippet.route('/snippets', methods=["POST"])
 def save_snippet():
   r = request.json
@@ -14,14 +13,23 @@ def save_snippet():
     'url': request.base_url + '/' + name,
     'name': name,
     'expires_at': datetime.now() + timedelta(seconds=expiry),
-    'snippet': snippet
+    'snippet': snippet,
+    'likes': 0
   }
   return dic[name], 201
 
 @snippet.route('/snippets/<name>', methods=["GET"])
 def get_snippet(name):
+  abort_or_extend(name)
+  return dic[name], 200
+
+@snippet.route('/snippets/<name>/like', methods=["GET"])
+def like_snippet(name):
+  abort_or_extend(name)
+  dic[name]['likes'] += 1
+  return dic[name], 200
+
+def abort_or_extend(name):
   if name not in dic or dic[name]['expires_at'] < datetime.now():
     abort(404)
   dic[name]['expires_at'] += timedelta(seconds=30)
-  dic[name] = snippet
-  return snippet, 200
